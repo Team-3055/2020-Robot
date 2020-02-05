7b8a13f13638aab1272b1e1f0ec7cdd8b164b432
Williams <connor.williams@austin.k12.mn.us>
2020-02-05 14:56:51 -0600

    Removed some imports to remove some "errors"

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -8,7 +8,7 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj.SpeedControllerGroup;
+//import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
--- src/main/java/frc/robot/subsystems/PneumaticSubsystem.java
+++ src/main/java/frc/robot/subsystems/PneumaticSubsystem.java
@@ -10,7 +10,7 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.Solenoid;
-import edu.wpi.first.wpilibj.DoubleSolenoid;
+//import edu.wpi.first.wpilibj.DoubleSolenoid;
 import frc.robot.Constants;
 
 public class PneumaticSubsystem extends SubsystemBase {
