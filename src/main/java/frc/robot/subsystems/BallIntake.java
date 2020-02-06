ca5d924a6e2733230941c078e3e9e5acf46b278b
Cheney Yao - Grade 9 <400188@austin.k12.mn.us>
2020-02-06 16:20:32 -0600

    Added BallIntake subsytem

---
+++ src/main/java/frc/robot/subsystems/BallIntake.java
@@ -0,0 +1,30 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class BallIntake extends SubsystemBase {
+  /**
+   * Creates a new ExampleSubsystem.
+   */ 
+
+  //Declared varibles for Pneumatics
+
+  //Boolean=true false, when calling BallRelease(boolean true/false) in code, 
+  //it will drop to here and open/close accordingly
+    // public void BallRelease(boolean ball) {
+    //   s1.set(ball);
+    // }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
