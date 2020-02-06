3c5ee5b2050ec761a51257182dc458a81c22bed7
Cheney Yao - Grade 9 <400188@austin.k12.mn.us>
2020-02-06 16:58:24 -0600

    Made IntakeBall Method

--- src/main/java/frc/robot/subsystems/BallIntake.java
+++ src/main/java/frc/robot/subsystems/BallIntake.java
@@ -18,6 +18,11 @@ public class BallIntake extends SubsystemBase {
   private final WPI_TalonSRX BeltMotor2 = new WPI_TalonSRX(Constants.bIntakeConstants.kBeltMotor2);
   private final WPI_VictorSPX LaunchMotor1 = new WPI_VictorSPX(Constants.bIntakeConstants.kLaunchMotor1);
   private final WPI_VictorSPX LaunchMotor2 = new WPI_VictorSPX(Constants.bIntakeConstants.kLaunchMotor2);
+  
+public void IntakeBall(float speed){
+  IntakeMotor.set(speed);
+}
+
   /**
    * Creates a new ExampleSubsystem.
    */ 
