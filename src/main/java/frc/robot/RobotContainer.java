24bdeac078e3a2da57fa8cb3f6a6560b67f530f1
Williams <23663468+connorwilliams-aps@users.noreply.github.com>
2020-02-10 15:47:03 -0600

    initialized miscsubsystem in robotcontainer

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -11,6 +11,7 @@ import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.subsystems.ColorSubsystem;
 import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.MiscSubsystem;
 //import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.commands.DefaultDrive;
 
@@ -33,6 +34,7 @@ public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final DriveSubsystem m_robotDrive = new DriveSubsystem();
   private final ColorSubsystem m_colorSensor = new ColorSubsystem();
+  private final MiscSubsystem m_miscSubsystem = new MiscSubsystem();
   //private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
   Joystick m_driverRController = new Joystick(Constants.OIConstants.kRightControllerPort);
   Joystick m_driverLController = new Joystick(Constants.OIConstants.kLeftControllerPort);
