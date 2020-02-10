913d993d8cbeb935a72afa26b94bd82ac84a758d
Williams <23663468+connorwilliams-aps@users.noreply.github.com>
2020-02-10 15:51:59 -0600

    Added wheelright command

--- src/main/java/frc/robot/commands/WheelRight.java
+++ src/main/java/frc/robot/commands/WheelRight.java
@@ -7,22 +7,23 @@
 
 package frc.robot.commands;
 
-import frc.robot.subsystems.ExampleSubsystem;
+
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.MiscSubsystem;
 
 /**
  * An example command that uses an example subsystem.
  */
-public class ExampleCommand extends CommandBase {
+public class WheelRight extends CommandBase {
   @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-  private final ExampleSubsystem m_subsystem;
+  private final MiscSubsystem m_subsystem;
 
   /**
    * Creates a new ExampleCommand.
    *
    * @param subsystem The subsystem used by this command.
    */
-  public ExampleCommand(ExampleSubsystem subsystem) {
+  public WheelRight(MiscSubsystem subsystem) {
     m_subsystem = subsystem;
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(subsystem);
@@ -36,11 +37,13 @@ public class ExampleCommand extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+    m_subsystem.ColorControl(-1);
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    m_subsystem.ColorControl(0);
   }
 
   // Returns true when the command should end.
