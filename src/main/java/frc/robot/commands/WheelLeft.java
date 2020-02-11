0501eed6b8262ec4a5587264916bc9d20eca667d
Williams <23663468+connorwilliams-aps@users.noreply.github.com>
2020-02-11 11:20:37 -0600

    Corrected wheelspin code

--- src/main/java/frc/robot/commands/WheelLeft.java
+++ src/main/java/frc/robot/commands/WheelLeft.java
@@ -7,8 +7,7 @@
 
 package frc.robot.commands;
 
-import frc.robot.subsystems.ExampleSubsystem;
-import frc.robot.subsystems.MiscSubsystem;
+import frc.robot.subsystems.ColorSubsystem;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 /**
@@ -16,14 +15,14 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
  */
 public class WheelLeft extends CommandBase {
   @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-  private final MiscSubsystem m_subsystem;
+  private final ColorSubsystem m_subsystem;
 
   /**
    * Creates a new ExampleCommand.
    *
    * @param subsystem The subsystem used by this command.
    */
-  public WheelLeft(MiscSubsystem subsystem) {
+  public WheelLeft(ColorSubsystem subsystem) {
     m_subsystem = subsystem;
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(subsystem);
--- src/main/java/frc/robot/commands/WheelRight.java
+++ src/main/java/frc/robot/commands/WheelRight.java
@@ -9,21 +9,21 @@ package frc.robot.commands;
 
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.subsystems.MiscSubsystem;
+import frc.robot.subsystems.ColorSubsystem;
 
 /**
  * An example command that uses an example subsystem.
  */
 public class WheelRight extends CommandBase {
   @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-  private final MiscSubsystem m_subsystem;
+  private final ColorSubsystem m_subsystem;
 
   /**
    * Creates a new ExampleCommand.
    *
    * @param subsystem The subsystem used by this command.
    */
-  public WheelRight(MiscSubsystem subsystem) {
+  public WheelRight(ColorSubsystem subsystem) {
     m_subsystem = subsystem;
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(subsystem);
