56942d74ae2944412c9f746e60081db6be3e7d18
Williams <23663468+connorwilliams-aps@users.noreply.github.com>
2020-02-17 10:52:05 -0600

    Inverted ball intake

--- src/main/java/frc/robot/commands/IntakeBall.java
+++ src/main/java/frc/robot/commands/IntakeBall.java
@@ -34,7 +34,7 @@ public class IntakeBall extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    m_subsystem.IntakeBall(-1);
+    m_subsystem.IntakeBall(1);
 
   }
 
