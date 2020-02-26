53fdd08eb45219236dc76d37f1d7f70b4a044e04
Team 3055 <team3055@austin.k12.mn.us>
2020-02-26 13:56:47 -0600

    Set belt speed back to .5

--- src/main/java/frc/robot/commands/BeltBallUp.java
+++ src/main/java/frc/robot/commands/BeltBallUp.java
@@ -34,7 +34,7 @@ public class BeltBallUp extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    m_subsystem.BeltBall(1);
+    m_subsystem.BeltBall(.5);
 
   }
 
